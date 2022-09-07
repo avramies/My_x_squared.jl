d164d36eb888e6779940c81aefd15b5aa521a807
avramies <aschwalb@mail.yu.edu>
2022-09-07 15:15:30 -0400

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using My_x_squared
 using Test
 
 @testset "My_x_squared.jl" begin
-    # Write your tests here.
+    @test My_x_squared.my_x_squared(2) == 4
+    @test My_x_squared.plusTwo(3) == 5
+    @test My_x_squared.plusThree(3) == 6
+    @test My_x_squared.my_x_squared(2) > 2
+    @test My_x_squared.plusTwo(-2) > -2
+    @test My_x_squared.plusThree(-5) == -2
 end
